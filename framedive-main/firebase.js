93f3633c05ec046271f4890b7a6ddc77ea2f40e5
Coder-X <divyanshsingh1754@gmail.com>
2024-04-20 22:46:36 +0530

    firebase

--- framedive-main/firebase.js
+++ framedive-main/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDpHCpXpJMNWFScR-VaWqo-vJRF7pxgjkg",
-  authDomain: "framedive-22a49.firebaseapp.com",
-  projectId: "framedive-22a49",
-  storageBucket: "framedive-22a49.appspot.com",
-  messagingSenderId: "102610384957",
-  appId: "1:102610384957:web:972c584b7b3dea7abf1254",
-  measurementId: "G-BEEQE0XDQV"
-};
+// const firebaseConfig = {
+//   apiKey: 
+//   authDomain: 
+//   projectId: 
+//   storageBucket: 
+//   messagingSenderId: 
+//   appId: 
+//   measurementId: 
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
